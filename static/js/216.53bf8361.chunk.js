11de2b8b9aae09143a82262b3112cb9b3e69501f
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-05-20 12:00:43 +0000

    Deploying to gh-pages from @ tomaszblock/goit-react-hw-05-movies@f4313cf3ba058d606b806005e7442e7486796556 🚀

--- static/js/216.53bf8361.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[216],{216:function(e,t,r){r.r(t);r(791);var o=r(87),n=r(184);t.default=function(){return(0,n.jsxs)("div",{children:[(0,n.jsx)("h1",{children:"404 - Not Found"}),(0,n.jsx)(o.rU,{to:"/",children:"Go to Home"})]})}}}]);
-//# sourceMappingURL=216.53bf8361.chunk.js.map
